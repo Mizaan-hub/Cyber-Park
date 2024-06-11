6d5e0d88f142004ab85022e8577c3c4e85cfc78a
Mizaan-hub <mizaanafandi@gmail.com>
2024-06-12 00:53:30 +0530

    Styled RightSidebar

---
+++ cyber-park/components/RightSidebar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const RightSidebar = () => {
+  return (
+    <div>
+      <section className="right_sidebar text-white-1">
+        Right Sidebar
+      </section>
+    </div>
+  )
+}
+
+export default RightSidebar
