5f9a79d80aaef895ef8dd97e9f87bf065788b9ec
Mizaan-hub <mizaanafandi@gmail.com>
2024-06-13 01:42:19 +0530

    Added laoder for button in create-podcast page

--- cyber-park/app/(root)/create-podcast/page.tsx
+++ cyber-park/app/(root)/create-podcast/page.tsx
@@ -28,6 +28,7 @@ import { useState } from "react";
 import { Textarea } from "@/components/ui/textarea";
 import GeneratePodcast from "@/components/GeneratePodcast";
 import GenerateThumbnail from "@/components/GenerateThumbnail";
+import { Loader } from "lucide-react";
 
 const voiceCategories = ["alloy", "shimmer", "nova", "echo", "fable", "onyx"];
 
@@ -38,6 +39,8 @@ const formSchema = z.object({
 
 const CreatePodcast = () => {
   const [voiceType, setVoiceType] = useState<string | null>(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -140,8 +143,22 @@ const CreatePodcast = () => {
             <GeneratePodcast />
             <GenerateThumbnail />
             <div className="mt-10 w-full">
-              <Button type="submit" className="text-16 w-full text-white-1 bg-orange-1 py-4 font-bold transition-all duration-500 hover:bg-black-1">
-                Create Podcast
+              <Button
+                type="submit"
+                className={
+                  isSubmitting
+                    ? "text-16 w-full text-white-1 bg-black-1 py-4 font-bold transition-all duration-500"
+                    : "text-16 w-full text-white-1 bg-orange-1 py-4 font-bold transition-all duration-500 hover:bg-black-1"
+                }
+              >
+                {isSubmitting ? (
+                  <>
+                    Submitting
+                    <Loader size={20} className="animate-spin ml-2" />
+                  </>
+                ) : (
+                  "Submit & Publish Podcast"
+                )}
               </Button>
             </div>
           </div>
